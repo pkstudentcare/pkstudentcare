60a2dcac4c97123941344535bebb0e62e8470963
pkstudentcare <st.care@prakanong.ac.th>
2026-07-01 00:47:13 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+/**
+ * Firebase Configuration
+ * NOTE: This file contains sensitive credentials. Do not commit it to version control.
+ */
+const firebaseConfig = {
+  apiKey: "AIzaSyA35VWP0HqWtlhpM30_pIPtrA_P48p3Jfw",
+  authDomain: "pkstudentcare-6cc57.firebaseapp.com",
+  databaseURL: "https://pkstudentcare-6cc57-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "pkstudentcare-6cc57",
+  storageBucket: "pkstudentcare-6cc57.firebasestorage.app",
+  messagingSenderId: "993494045553",
+  appId: "1:993494045553:web:a0b38a0b30b9fb5ba41cd0",
+  measurementId: "G-4C5KQ9N0LL"
+};
